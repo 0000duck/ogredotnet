0f82ed8bbac283e7aa8b8181bc8db5f1c75a510e
jeffames <jeffames@706876d2-8941-0410-8d4b-891fc427835f>
2008-03-09 08:51:07 +0000

    Added build script for Linux / OS X

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This should build OgreDotNet under Linux and OS X
+
+clean() {
+    rm -rf CMakeCache.txt cmake_install.cmake CMakeFiles *.sln *.build Makefile
+    rm -rf Math3D/*.csproj Math3D/*.build Math3D/*.user Math3D/bin
+    rm -rf *.so bin/*.dll
+    rm -rf OgreNet/*_wrap.* OgreNet/*.build OgreNet/*.user OgreNet/*.csproj OgreNet/bin
+
+    # To avoid deleting OgreNet/AssemblyInfo.cs, rename it temporarily
+    mv OgreNet/AssemblyInfo.cs OgreNet/AssemblyInfo.cs.cleaning_script.temp
+    rm -rf OgreNet/*.cs
+    mv OgreNet/AssemblyInfo.cs.cleaning_script.temp OgreNet/AssemblyInfo.cs
+}
+
+build_swig() {
+    cmake -G "Unix Makefiles"
+    make
+}
+
+build_csharp() {
+    ./runprebuild.sh
+    nant
+}
+
+clean
+build_swig
+build_csharp
