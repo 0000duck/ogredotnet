344defb44e1424f891becc8eaa843e18164716ec
jeffames <jeffames@706876d2-8941-0410-8d4b-891fc427835f>
2008-04-02 04:48:44 +0000

    Updated build.sh with new directory structure.

--- build.sh
+++ build.sh
@@ -7,11 +7,7 @@ clean() {
     rm -rf Math3D/*.csproj Math3D/*.build Math3D/*.user Math3D/bin
     rm -rf *.so bin/*.dll
     rm -rf OgreNet/*_wrap.* OgreNet/*.build OgreNet/*.user OgreNet/*.csproj OgreNet/bin
-
-    # To avoid deleting OgreNet/AssemblyInfo.cs, rename it temporarily
-    mv OgreNet/AssemblyInfo.cs OgreNet/AssemblyInfo.cs.cleaning_script.temp
-    rm -rf OgreNet/*.cs
-    mv OgreNet/AssemblyInfo.cs.cleaning_script.temp OgreNet/AssemblyInfo.cs
+    rm -rf OgreNet/build/*.cs
 }
 
 build_swig() {
